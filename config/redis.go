c3843bce6cdc590034bd7004311a7726f0ab46ef
mba <mba@host.local>
2022-05-12 19:57:30 +0800

    从配置文件加载配置 & DB 初始化

--- config/redis.go
+++ config/redis.go
@@ -3,21 +3,23 @@ package config
 import (
 	G "github.com/NoCLin/douyin-backend-go/config/global"
 	"github.com/go-redis/redis"
-	"log"
 )
 
 //获得Redis客户端连接
 func initRedis() error {
 
-	redisDB := redis.NewClient(&redis.Options{})
+	redisDB := redis.NewClient(&redis.Options{
+		Password: G.Config.Redis.Password,
+		Addr:     G.Config.Redis.Addr,
+		DB:       G.Config.Redis.DB,
+	})
 	_, err := redisDB.Ping().Result() //ping通了才代表客户端连接成功
 
 	if err != nil {
-		panic("failed to migrate redis")
+		panic("failed to connect redis")
 	}
 
 	G.RedisDB = redisDB
-	log.Println("The redis was connect successfully")
 	return nil
 
 }
