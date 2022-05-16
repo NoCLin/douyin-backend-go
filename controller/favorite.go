f14da5c731363b52ef5ca62a947b7c4d4aaa4057
BoyunWang <3368760963@qq.com>
2022-05-16 23:35:10 +0800

    完善favorite

--- controller/favorite.go
+++ controller/favorite.go
@@ -6,7 +6,6 @@ import (
 	"github.com/NoCLin/douyin-backend-go/utils"
 	"github.com/gin-gonic/gin"
 	"net/http"
-
 )
 
 // FavoriteAction no practical effect, just check if token is valid
@@ -24,23 +23,23 @@ func FavoriteAction(c *gin.Context) {
 	//relationKey := utils.GetUserRelationKey(userId)
 	//followerKey := utils.GetUserFollowerKey(toUserId)
 	userFavoriteKey := utils.GetVideoFavoriteKey(userId)
-	videoBeFavouriteKey :=utils.GetVideoFavoriteNumKey(videoId)
+	videoBeFavouriteKey := utils.GetVideoFavoriteNumKey(videoId)
 	if actionType == "1" {
 		pipe := global.RedisDB.TxPipeline()
 		//用户喜欢的视频列表
-		global.RedisDB.SAdd(userFavoriteKey, videoId)
+		global.RedisDB.SAdd(c, userFavoriteKey, videoId)
 		//视频被哪些粉丝点赞
-		global.RedisDB.SAdd(videoBeFavouriteKey, userId)
-		_, err := pipe.Exec()
+		global.RedisDB.SAdd(c, videoBeFavouriteKey, userId)
+		_, err := pipe.Exec(c)
 		if err != nil {
 		}
 	} else if actionType == "2" {
 		pipe := global.RedisDB.TxPipeline()
 		//用户喜欢的视频列表
-		global.RedisDB.SRem(userFavoriteKey, videoId)
+		global.RedisDB.SRem(c, userFavoriteKey, videoId)
 		//视频被哪些粉丝点赞
-		global.RedisDB.SRem(videoBeFavouriteKey, userId)
-		_, err := pipe.Exec()
+		global.RedisDB.SRem(c, videoBeFavouriteKey, userId)
+		_, err := pipe.Exec(c)
 		if err != nil {
 		}
 	} else {
@@ -53,51 +52,43 @@ func FavoriteAction(c *gin.Context) {
 func FavoriteList(c *gin.Context) {
 	token := c.Query("token")
 	userId := c.Query("user_id")
+
 	if _, exist := usersLoginInfo[token]; exist {
 		c.JSON(http.StatusOK, model.Response{StatusCode: 0})
 	} else {
 		c.JSON(http.StatusOK, model.Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
 	}
+
 	userFavoriteKey := utils.GetVideoFavoriteKey(userId)
-	es, _ := global.RedisDB.SMembers(userFavoriteKey).Result()
-	len := len(es)
-	var favorite_list = make([]model.Video, len, len)
-	for i := 0; i < len; i++ {
+	es, _ := global.RedisDB.SMembers(c, userFavoriteKey).Result()
+	length := len(es)
+	var favorite_list = make([]model.VideoResponse, length, length)
+
+	for i := 0; i < length; i++ {
 		curVideoFavoriteNum := utils.GetVideoFavoriteNumKey(es[i])
-		//curFollowerkey := utils.GetUserFollowerKey(es[i])
-		videoFavoriteNum, _ := global.RedisDB.SCard(curVideoFavoriteNum).Result()
-		//followerCount, _ := global.RedisDB.SCard(curFollowerkey).Result()
+		videoFavoriteNum, _ := global.RedisDB.SCard(c, curVideoFavoriteNum).Result()
+
 		var video model.Video
-
-
-
 		global.DB.Where("id = ?", es[i]).Find(&video)
 		favorite_list[i] = model.VideoResponse{
-			Video:model.Video{
+			Video: model.Video{
 				AuthorID: video.AuthorID,
 				Author:   video.Author,
 				PlayUrl:  video.PlayUrl,
 				CoverUrl: video.CoverUrl,
 			},
-			FavoriteCount :videoFavoriteNum,
-			CommentCount  :0,
-			IsFavorite :true,
-			}
-
+			FavoriteCount: videoFavoriteNum,
+			CommentCount:  0,
+			IsFavorite:    true,
+		}
 	}
-		c.JSON(http.StatusOK, model.VideoListResponse{
-			Response: model.Response{
-				StatusCode: 0,
-				StatusMsg:  "获取点赞列表成功",
-			},
-			VideoList:favorite_list,
-		})
+	c.JSON(http.StatusOK, model.VideoListResponse{
+		Response: model.Response{
+			StatusCode: 0,
+			StatusMsg:  "获取点赞列表成功",
+		},
+		VideoList: favorite_list,
+	})
 
-
-
-		return
-	}
-
-
-
+	return
 }
